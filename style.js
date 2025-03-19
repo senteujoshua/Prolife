c41ba19738192eb871b266ad59033ae3afc5d9b4
WairiaGracee <gjblessed12@gmail.com>
2025-03-19 12:28:17 +0300

    responsive header

--- style.js
+++ style.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementsById("menu").addEventListener('click', function(){
-        const nav = document.getElementById('nav')
-        nav.classList.add('show')
-    })
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu");
+    const closeIcon = document.querySelector(".close-icon");
+    const navMenu = document.querySelector(".nav-ul");
 
-})
+    closeIcon.style.display = 'none';
+    menuIcon.addEventListener("click", function () {
+        navMenu.classList.toggle("show");
+        menuIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+    });
+
+    closeIcon.addEventListener("click", function () {
+        navMenu.classList.remove("show");
+        menuIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
+    });
+});
+
+
