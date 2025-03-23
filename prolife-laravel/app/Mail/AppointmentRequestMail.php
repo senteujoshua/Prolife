ace87e45c1defc259bc97a605da3522d48616fbd
senteujoshua <117025098+senteujoshua@users.noreply.github.com>
2025-03-23 12:09:10 +0300

    done

---
+++ prolife-laravel/app/Mail/AppointmentRequestMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AppointmentRequestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    public function build()
+    {
+        return $this->subject('New Appointment Request')
+            ->view('emails.appointment_request')
+            ->with('details', $this->details);
+    }
+}
---
+++ prolife-laravel/resources/views/appointment_request.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Appointment Request</title>
+</head>
+<body>
+<h3>New Appointment Request</h3>
+<p><strong>Name:</strong> {{ $details['name'] }}</p>
+<p><strong>Email:</strong> {{ $details['email'] }}</p>
+<p><strong>Phone:</strong> {{ $details['phone'] }}</p>
+<p><strong>Services:</strong> {{ $details['services'] }}</p>
+<p><strong>Preferred Date:</strong> {{ $details['date'] }}</p>
+</body>
+</html>
